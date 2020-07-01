8d8496d27c0b37dafc1578c7c7aa70c79b174419
VADDAVALLI SAI SITA RAM <saisitaramvaddavalli@gmail.com>
2020-07-01 22:13:43 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ import "weather-icons/css/weather-icons.css";
       //get city and country name from input fields
       const country=e.target.elements.country.value;
       const city=e.target.elements.city.value;
-      
+
      if(country && city){
        try{
        const result=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},q=${country}&appid=${api_key}`);
@@ -97,7 +97,7 @@ import "weather-icons/css/weather-icons.css";
 
        //set icons
        this.getWeatherIcon(this.weathericon,response.weather[0].id);
-      // console.log(this.state.icon);
+       console.log(this.state.icon);
        console.log(response);
       }catch(error){
         alert('City you are searching is not found!');
