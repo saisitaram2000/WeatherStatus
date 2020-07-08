1651329058701f2af0af8e7cee45e24e0e13bca3
saisitaram2000 <saisitaramvaddavalli@gmail.com>
2020-07-08 21:01:22 +0530

    Updates

---
+++ precache-manifest.a9a0e13b1562b551eda0a9f52b591858.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a53b2e6bf6bf902d672bfffaca1f7a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4a4a2032214e89f5e6f",
+    "url": "/static/css/2.513edf02.chunk.css"
+  },
+  {
+    "revision": "bb737678e7272bd0929b",
+    "url": "/static/css/main.9ccc067f.chunk.css"
+  },
+  {
+    "revision": "a4a4a2032214e89f5e6f",
+    "url": "/static/js/2.21490eeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.21490eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb737678e7272bd0929b",
+    "url": "/static/js/main.b2f218b7.chunk.js"
+  },
+  {
+    "revision": "3772c0f81f7ab39a145b",
+    "url": "/static/js/runtime-main.06cda749.js"
+  },
+  {
+    "revision": "a22f258357798b97bb4811b675970838",
+    "url": "/static/media/img.a22f2583.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
